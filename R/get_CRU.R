d61e001c9981159c87cefed43b128c439b1afdc4
Adam H. Sparks <adamhsparks@gmail.com>
2016-12-13 18:03:50 +1000

    Comment get_CRU.R; Fix bugs in tibble creation

--- R/get_CRU.R
+++ R/get_CRU.R
@@ -11,7 +11,6 @@
            frs,
            wnd,
            elv) {
-
     CRU_list <- NULL
     month_names <-
       c("jan",
@@ -26,120 +25,166 @@
         "oct",
         "nov",
         "dec")
-
+ # dtr -------------------------------------------------------------------------
     if (dtr == TRUE | tmn == TRUE | tmx == TRUE) {
       dtr <- readr::read_table(
         "http://www.cru.uea.ac.uk/cru/data/hrg/tmc/grid_10min_dtr.dat.gz",
-        col_names = FALSE)
+        col_names = FALSE
+      )
+      names(dtr_df) <- c("lat", "lon", month_names)
       dtr_df <-
         dtr_df %>%
-        tidyr::gather(key = "month", value = "tmp", dplyr::everything(), -c(lat, lon))
+        tidyr::gather(key = "month",
+                      value = "dtr",
+                      dplyr::everything(),
+                      -c(lat, lon))
     }
-
+    # tmp ----------------------------------------------------------------------
     if (tmp == TRUE | tmn == TRUE | tmx == TRUE) {
       tmp_df <- readr::read_table(
         "http://www.cru.uea.ac.uk/cru/data/hrg/tmc/grid_10min_tmp.dat.gz",
-        col_names = FALSE)
+        col_names = FALSE
+      )
+      names(tmp_df) <- c("lat", "lon", month_names)
       tmp_df <-
         tmp_df %>%
-        tidyr::gather(key = "month", value = "tmp", dplyr::everything(), -c(lat, lon))
-        }
-
+        tidyr::gather(key = "month",
+                      value = "tmp",
+                      dplyr::everything(),
+                      -c(lat, lon))
+    }
+    # pre ----------------------------------------------------------------------
     if (pre == TRUE) {
       pre_df <- readr::read_table(
         "http://www.cru.uea.ac.uk/cru/data/hrg/tmc/grid_10min_pre.dat.gz",
-        col_names = FALSE)
+        col_names = FALSE
+      )
+      # split the data frame into two, otherwise we get errors on C stack usage
       pre_df1 <- pre_df[, c(1:14)]
       pre_df2 <- pre_df[, c(1:2, 15:26)]
-      names(pre_df1) <- names(pre_df2) <- c("lat", "lon", month_names)
+      names(pre_df1) <-
+        names(pre_df2) <- c("lat", "lon", month_names)
       pre_df1 <-
         pre_df1 %>%
-        tidyr::gather(key = "month", value = "pre", dplyr::everything(), -c(lat, lon))
+        tidyr::gather(key = "month",
+                      value = "pre",
+                      dplyr::everything(),
+                      -c(lat, lon))
       pre_df2 <-
         pre_df2 %>%
-        tidyr::gather(key = "month", value = "pre_cv", dplyr::everything(), -c(lat, lon))
+        tidyr::gather(key = "month",
+                      value = "pre_cv",
+                      dplyr::everything(),
+                      -c(lat, lon))
       pre_df <- dplyr::bind_cols(pre_df1, pre_df2[, 4])
     }
-
+    # reh ----------------------------------------------------------------------
     if (reh == TRUE) {
       reh_df <- readr::read_table(
         "http://www.cru.uea.ac.uk/cru/data/hrg/tmc/grid_10min_reh.dat.gz",
-        col_names = FALSE)
-
+        col_names = FALSE
+      )
+      names(reh_df) <- c("lat", "lon", month_names)
       reh_df <-
         reh_df %>%
-        tidyr::gather(key = "month", value = "tmp", dplyr::everything(), -c(lat, lon))
+        tidyr::gather(key = "month",
+                      value = "reh",
+                      dplyr::everything(),
+                      -c(lat, lon))
     }
-
+    # elv ----------------------------------------------------------------------
     if (elv == TRUE) {
       elv_df <- readr::read_table(
         "http://www.cru.uea.ac.uk/cru/data/hrg/tmc/grid_10min_elv.dat.gz",
-        col_names = FALSE)
-
-      elv_df <-
-        elv_df %>%
-        tidyr::gather(key = "month", value = "tmp", dplyr::everything(), -c(lat, lon))
+        col_names = FALSE
+      )
+      names(elv_df) <- c("lat", "lon", "elv_m")
+      elv_df[, 3] <- elv_df[, 3] * 1000
     }
-
+    # sunp ----------------------------------------------------------------------
     if (sunp == TRUE) {
       sunp_df <- readr::read_table(
         "http://www.cru.uea.ac.uk/cru/data/hrg/tmc/grid_10min_sunp.dat.gz",
-        col_names = FALSE)
-
+        col_names = FALSE
+      )
+      names(sunp_df) <- c("lat", "lon", month_names)
       sunp_df <-
         sunp_df %>%
-        tidyr::gather(key = "month", value = "tmp", dplyr::everything(), -c(lat, lon))
+        tidyr::gather(key = "month",
+                      value = "sunp",
+                      dplyr::everything(),
+                      -c(lat, lon))
     }
-
+    # wnd ----------------------------------------------------------------------
     if (wnd == TRUE) {
       wnd_df <- readr::read_table(
         "http://www.cru.uea.ac.uk/cru/data/hrg/tmc/grid_10min_wnd.dat.gz",
-        col_names = FALSE)
-
+        col_names = FALSE
+      )
+      names(wnd_df) <- c("lat", "lon", month_names)
       wnd_df <-
         wnd_df %>%
-        tidyr::gather(key = "month", value = "tmp", dplyr::everything(), -c(lat, lon))
+        tidyr::gather(key = "month",
+                      value = "wnd",
+                      dplyr::everything(),
+                      -c(lat, lon))
     }
-
+    # frs ----------------------------------------------------------------------
     if (frs == TRUE) {
       frs_df <- readr::read_table(
         "http://www.cru.uea.ac.uk/cru/data/hrg/tmc/grid_10min_frs.dat.gz",
-        col_names = FALSE)
-
+        col_names = FALSE
+      )
+      names(frs_df) <- c("lat", "lon", month_names)
       frs_df <-
         frs_df %>%
-        tidyr::gather(key = "month", value = "tmp", dplyr::everything(), -c(lat, lon))
+        tidyr::gather(key = "month",
+                      value = "frs",
+                      dplyr::everything(),
+                      -c(lat, lon))
     }
-
+    # rd0 ----------------------------------------------------------------------
     if (rd0 == TRUE) {
       rd0_df <- readr::read_table(
         "http://www.cru.uea.ac.uk/cru/data/hrg/tmc/grid_10min_rd0.dat.gz",
-        col_names = FALSE)
-
+        col_names = FALSE
+      )
+      names(rd0_df) <- c("lat", "lon", month_names)
       rd0_df <-
         rd0_df %>%
-        tidyr::gather(key = "month", value = "tmp", dplyr::everything(), -c(lat, lon))
+        tidyr::gather(key = "month",
+                      value = "rd0",
+                      dplyr::everything(),
+                      -c(lat, lon))
     }
-
+    # calculate tmx ------------------------------------------------------------
     if (isTRUE(tmx)) {
       # calculate tmax and tmin from tmp and dtr
       tmx <- tmp[, c(3:14)] + (0.5 * dtr[, c(3:14)])
       tmx <- dplyr::bind_cols(tmp[, 1:2], tmx)
     }
-
+    # calculate tmn ------------------------------------------------------------
     if (isTRUE(tmn)) {
       tmn <- tmp[, c(3:14)] - (0.5 * dtr[, c(3:14)])
       tmn <- dplyr::bind_cols(tmp[, c(1:2)], tmn)
     }
 
-    if (isTRUE(elv)) {
-      # convert elevation to metres
-      elv <- elv / 1000
-    }
+    # create list of data frames -----------------------------------------------
 
     CRU_list <-
-      list(dtr_df, pre_df, reh_df, tmn_df, tmp_df, tmx_df, rd0_df, elv_df,
-           frs_df, wnd_df, sunp_df)
+      list(
+        dtr_df,
+        pre_df,
+        reh_df,
+        tmn_df,
+        tmp_df,
+        tmx_df,
+        rd0_df,
+        elv_df,
+        frs_df,
+        wnd_df,
+        sunp_df
+      )
     return(CRU_list)
   }
 
