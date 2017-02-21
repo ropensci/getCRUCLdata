5739daa4fcac8ef5e4c5b25139dab8c883b2bc81
Adam H. Sparks <adamhsparks@gmail.com>
2017-02-21 20:32:14 +1000

    Update documentation
    Help file for `getCRUCLdata` did not exist

--- R/getCRUCLdata.R
+++ R/getCRUCLdata.R
@@ -1,6 +1,6 @@
-#' getCRUdata: Download and Create a Data Frame or Raster Stack Object of CRU CL2.0 Climatology Variables
+#' getCRUCLdata: Download and Create a Data Frame or Raster Stack Object of CRU CL2.0 Climatology Variables
 #'
-#' The getCRUdata package provides functions that automate downloading and
+#' The getCRUCLdata package provides functions that automate downloading and
 #' importing CRU CL2.0 climatology data into R, facilitates the calculation of
 #' minimum temperature and maximum temperature, converts elevation from
 #' kilometres to metres and formats the data into a tidy data frame or a list
@@ -13,7 +13,7 @@
 #'
 #' @docType package
 #'
-#' @name getCRUdata
+#' @name getCRUCLdata
 #'
 #' @seealso \code{\link{create_CRU_df}}
 #'
