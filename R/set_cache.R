b171b43c27fc86ab5c35beb4ed804017d525c083
Adam H. Sparks <adamhsparks@gmail.com>
2017-06-26 13:56:38 +1000

    Revert "Fix bug in setting/creating cache directory on Windows"
    This reverts commit ac3aa616f6c1feb0d482effa6f8f3b9735d42144.

--- R/set_cache.R
+++ R/set_cache.R
@@ -2,15 +2,11 @@
 .set_cache <- function(cache) {
   if (isTRUE(cache)) {
     cache_dir <- rappdirs::user_cache_dir("getCRUCLdata")
-    if (!dir.exists(cache_dir)) {
-      dir.create(rappdirs::user_cache_dir(appname = "getCRUCLdata",
-                                          appauthor = "getCRUCLdata"),
-                 recursive = TRUE)
+    if (!file.exists(cache_dir)) {
+      dir.create(cache_dir)
     }
   } else {
     cache_dir <- tempdir()
   }
   return(cache_dir)
 }
-
-
