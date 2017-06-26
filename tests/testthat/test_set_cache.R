9b407a9ab9390db1459884af7cea126677dd5fa8
Adam H. Sparks <adamhsparks@gmail.com>
2017-06-26 10:18:08 +1000

    Add additional test for cache_dir
    Test that `tempdir()` is used if cache == FALSE

--- tests/testthat/test_set_cache.R
+++ tests/testthat/test_set_cache.R
@@ -4,9 +4,15 @@
 test_that("test that set_cache creates a cache directory if none exists", {
   skip_on_cran()
   unlink(rappdirs::user_cache_dir("getCRUCLdata"), recursive = TRUE)
-  cache = TRUE
+  cache <- TRUE
   .set_cache(cache)
   expect_true(file.exists(file.path(rappdirs::user_cache_dir("getCRUCLdata"))))
   # cleanup
   unlink(rappdirs::user_cache_dir("climcropr"), recursive = TRUE)
+})
+
+test_that("test that set_cache uses tempdir() if caching == FALSE", {
+  cache <- FALSE
+  cache_dir <- .set_cache(cache)
+  expect_true(cache_dir == tempdir())
 })
