4c7a9881ba9e6e5a19f5734cf8d06aed85328e08
Adam H. Sparks <adamhsparks@icloud.com>
2024-12-15 07:31:09 +0800

    Convert Roxygen to Markdown

--- R/manage_cached_files.R
+++ R/manage_cached_files.R
@@ -1,30 +1,30 @@
-#' @title Manage locally cached CRU CL v. 2.0 files
+#' Manage locally cached CRU CL v. 2.0 files
 #'
-#' @description Manage cached \CRANpkg{getCRUCLdata} files with \CRANpkg{hoardr}
+#' Manage cached \pkg{getCRUCLdata} files with \CRANpkg{hoardr}
 #'
 #' @export
 #' @name manage_cache
 #'
 #' @details The default cache directory is
-#' \code{file.path(rappdirs::user_cache_dir(), "R/getCRUCLdata")}, but you can
-#' set your own path using \code{manage_cache$cache_path_set()}
+#' `tools::R_user_dir(package = "getCRUCLdata")`, but you can
+#' set your own path using `manage_cache$cache_path_set()`
 #'
-#' \code{manage_cache$cache_delete} only accepts one file name, while
-#' \code{manage_cache$cache_delete_all}
+#' `manage_cache$cache_delete` only accepts one file name, while
+#' `manage_cache$cache_delete_all`
 #' does not accept any names, but deletes all files. For deleting many specific
-#' files, use \code{manage_cache$cache_delete} in an
-#' \code{\link[base]{lapply}()} type call.
+#' files, use `manage_cache$cache_delete` in an
+#' [base::lapply()] type call.
 #'
 #' @section Useful user functions:
 #' \itemize{
-#'  \item \code{manage_cache$cache_path_get()} - get cache path
-#'  \item \code{manage_cache$cache_path_set()} - set cache path
-#'  \item \code{manage_cache$list()} - returns a character vector of full
+#'  \item `manage_cache$cache_path_get()` - get cache path
+#'  \item `manage_cache$cache_path_set()` - set cache path
+#'  \item `manage_cache$list()` - returns a character vector of full
 #'  path file names
-#'  \item \code{manage_cache$files()} - returns file objects with metadata
-#'  \item \code{manage_cache$details()} - returns files with details
-#'  \item \code{manage_cache$delete()} - delete specific files
-#'  \item \code{manage_cache$delete_all()} - delete all files, returns
+#'  \item `manage_cache$files()` - returns file objects with metadata
+#'  \item `manage_cache$details()` - returns files with details
+#'  \item `manage_cache$delete()` - delete specific files
+#'  \item `manage_cache$delete_all()` - delete all files, returns
 #'  nothing
 #' }
 #'
