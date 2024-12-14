4c4231d02deeaa2f4e2d8886fbdfc2d4bd087979
Adam H. Sparks <adamhsparks@icloud.com>
2024-12-15 07:31:40 +0800

    Use proper R user cache directory location

--- R/zzz.R
+++ R/zzz.R
@@ -8,12 +8,11 @@ manage_cache <- NULL # nocov start
       utils::globalVariables(c("."))
 
       x <- hoardr::hoard()
-      x$cache_path_set(path = "getCRUCLdata", type = "user_cache_dir")
+      x$cache_path_set(path = "getCRUCLdata",
+                       prefix = "org.R-project.R/R",
+                       type = "user_cache_dir")
       manage_cache <<- x
     }
   }
-
-#' @import data.table
-NULL
 
 # nocov end
