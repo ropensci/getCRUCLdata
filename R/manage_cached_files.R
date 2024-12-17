ec3215543d184a34468d754d405841a395ddbda0
Adam H. Sparks <adam.sparks@curtin.edu.au>
2024-12-17 10:25:34 +0800

    tidying Roxygen

--- R/manage_cached_files.R
+++ R/manage_cached_files.R
@@ -6,8 +6,8 @@
 #' @name manage_cache
 #'
 #' @details The default cache directory is
-#' `tools::R_user_dir(package = "getCRUCLdata")`, but you can
-#' set your own path using `manage_cache$cache_path_set()`.
+#' `tools::R_user_dir(package = "getCRUCLdata")`, but you can set your own path
+#' using `manage_cache$cache_path_set()`.
 #'
 #' `manage_cache$cache_delete` only accepts one file name, while
 #' `manage_cache$cache_delete_all`
