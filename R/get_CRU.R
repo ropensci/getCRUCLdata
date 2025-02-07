621bbe1222b34b691dceace3ab93508a24ce08bf
Adam H. Sparks <adam.sparks@curtin.edu.au>
2025-02-07 15:48:48 +0800

    document internal function

--- R/get_CRU.R
+++ R/get_CRU.R
@@ -1,4 +1,25 @@
-#' @noRd
+#' Downloads and formats CRU CL 2.0 data
+#'
+#' @param pre Logical. If TRUE, downloads precipitation data.
+#' @param pre_cv Logical. If TRUE, downloads precipitation data.
+#' @param rd0 Logical. If TRUE, downloads runoff data.
+#' @param tmp Logical. If TRUE, downloads temperature data.
+#' @param dtr Logical. If TRUE, downloads diurnal temperature range data.
+#' @param reh Logical. If TRUE, downloads relative humidity data.
+#' @param tmn Logical. If TRUE, downloads minimum temperature data.
+#' @param tmx Logical. If TRUE, downloads maximum temperature data.
+#' @param sunp Logical. If TRUE, downloads sunshine data.
+#' @param frs Logical. If TRUE, downloads frost day frequency data.
+#' @param wnd Logical. If TRUE, downloads wind speed data.
+#' @param elv Logical. If TRUE, downloads elevation data.
+#' @param cache_dir Character. Path to the cache directory.
+#'
+#' Handles the downloading of CRU CL 2.0 data. This function is called by
+#' [get_cru_df] and [get_cru_stack]. It is not intended to be called directly.
+#'
+#' @returns A data.table with the requested data.
+#'
+#' @dev
 .get_CRU <-
   function(pre,
            pre_cv,
@@ -23,7 +44,7 @@
     frs_file <- "grid_10min_frs.dat.gz"
     rd0_file <- "grid_10min_rd0.dat.gz"
 
-    # check if pre_cv or tmx/tmn (derived) are true, make sure proper ----------
+    # check if pre_cv or tmx/tmn (derived) are true, make sure proper ---------
     # parameters set TRUE
     if (pre_cv) {
       pre <- TRUE
@@ -32,7 +53,7 @@
     if (any(tmn, tmx)) {
       dtr <- tmp <- TRUE
     }
-    # create object list to filter downloads -----------------------------------
+    # create object list to filter downloads ----------------------------------
     object_list <- c(dtr, tmp, reh, elv, pre, sunp, wnd, frs, rd0)
 
     files <-
@@ -61,7 +82,7 @@
         "rd0_file"
       )
 
-    # filter downloaded --------------------------------------------------------
+    # filter downloaded -------------------------------------------------------
     # which files are being requested?
     files <- files[which(object_list)]
 
@@ -72,7 +93,7 @@
     # which files requested need to be downloaded
     dl_files <- files[!(files %in% cache_dir_contents)]
 
-    # download files -----------------------------------------------------------
+    # download files ----------------------------------------------------------
     if (length(dl_files) > 0) {
       CRU_url <- "https://crudata.uea.ac.uk/cru/data/hrg/tmc/"
       dl_files <- as.list(paste0(CRU_url, dl_files))
