06f5453e316eb258b5c4be44e3bce07cd6c7d164
Adam H. Sparks <adamhsparks@icloud.com>
2026-05-03 21:02:12 +0800

    fix: add constants.R for shared vectors of values

---
+++ R/constants.R
@@ -0,0 +1,26 @@
+.cru_month_names <- c(
+  "jan",
+  "feb",
+  "mar",
+  "apr",
+  "may",
+  "jun",
+  "jul",
+  "aug",
+  "sep",
+  "oct",
+  "nov",
+  "dec"
+)
+
+CRU_FILES <- c(
+  pre = "grid_10min_pre.dat.gz",
+  rd0 = "grid_10min_rd0.dat.gz",
+  tmp = "grid_10min_tmp.dat.gz",
+  dtr = "grid_10min_dtr.dat.gz",
+  reh = "grid_10min_reh.dat.gz",
+  sunp = "grid_10min_sunp.dat.gz",
+  frs = "grid_10min_frs.dat.gz",
+  wnd = "grid_10min_wnd.dat.gz",
+  elv = "grid_10min_elv.dat.gz"
+)
